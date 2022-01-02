4ad545b469288de018115147d1fb1e025e3341ca
Callum 🍺 <callumimacmillan@gmail.com>
2022-01-02 20:06:54 +0000

    Added some more info and worked a bit on jupyter notebook

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # Cocktails
 
+Setting up the environment
+
 ```
 conda env create -f environment.yml
 conda activate cocktails
+```
+
+Executing file
+
+```
+python ./src/util.py
+```
+
+Opening Jupyter notebook
+
+https://jupyter.org/install
+
+```
+jupyter notebook 
 ```
 
--- src/util.py
+++ src/util.py
@@ -1,8 +1,14 @@
+import numpy as np 
+
 
 def sum(a, b): 
     return a + b
 
 
 print(sum(1, 2))
+
+# TODO https://www.reddit.com/r/algotrading/comments/92nzp2/question_backtesting_in_python/
+# TODO https://github.com/mementum/backtrader
+# TODO https://www.reddit.com/r/algotrading/wiki/index
 
 
